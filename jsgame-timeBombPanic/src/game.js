214ff0eb43d64198577eca83955387a9a9abe4d4
zhangping168@gmail.com <zhangping168@gmail.com>
2017-07-24 01:40:16 -0400

    Add alien movement functions

--- jsgame-timeBombPanic/src/game.js
+++ jsgame-timeBombPanic/src/game.js
@@ -53,7 +53,7 @@
 	var sprites = [],
 		boxes = [],
 		bombs = [],
-		messages=[],
+		messages = [],
 		alien = null,
 		assetsToLoad = [];
 
@@ -62,6 +62,54 @@
 		gameOverDisplay = null,
 		gameOverMessage = null,
 		timerMessage = null;
+
+	//keyboard movement
+	var arrowLeft = 37,
+		arrowUp = 38,
+		arrowRight = 39,
+		arrowDown = 40,
+		moveLeft = false,
+		moveRight = false,
+		moveUp = false,
+		moveDown = false;
+
+	window.addEventListener('keydown',keydownHandler,false);
+	window.addEventListener('keyup',keyupHandler,false);
+
+	function keydownHandler(event){
+		switch(event.keyCode){
+			case arrowLeft:
+				moveLeft=true;
+				break;
+			case arrowRight:
+				moveRight=true;
+				break;
+			case arrowUp:
+				moveUp=true;
+				break;
+			case arrowDown:
+				moveDown=true;
+				break;
+		}
+
+	}//end of keydownHandler function
+
+	function keyupHandler(event){
+		switch(event.keyCode){
+			case arrowLeft:
+				moveLeft=false;
+				break;
+			case arrowRight:
+				moveRight=false;
+				break;
+			case arrowUp:
+				moveUp=false;
+				break;
+			case arrowDown:
+				moveDown=false;
+				break;
+		}
+	}//end of keyupHandler function
 
 	//load tilesheet image
 	var image = new Image();
@@ -89,7 +137,10 @@
 		}
 	} //end of loadHandler function
 
+	update();
+
 	function update() {
+		window.requestAnimationFrame(update, canvas);
 
 		switch (gameState) {
 			case LOADING:
@@ -108,10 +159,28 @@
 				endGame();
 				break;
 		} //end of switch conditions
+
 		render();
+
 	} //end of update function
 
 	function render() {
+		//clear canvas before drawing anything
+		drawingSurface.clearRect(0, 0, canvas.width, canvas.height);
+
+		if (sprites.length) {
+			for (var i = 0; i < sprites.length; i++) {
+				var sprite = sprites[i];
+				if (sprite.visible) {
+
+					drawingSurface.drawImage(image,
+						sprite.sourceX, sprite.sourceY, sprite.sourceWidth, sprite.sourceHeight,
+						Math.floor(sprite.x), Math.floor(sprite.y), sprite.width, sprite.height
+					);
+				}
+
+			} //end of for loop
+		}
 
 	} //end of render function
 
@@ -122,7 +191,7 @@
 				if (currentTile !== EMPTY) {
 					//find the tile's X and Y position on tilesheet
 					var tileSheetX = Math.floor((currentTile - 1) % tilesheetColumns) * SIZE;
-					var tileSheeY = Math.floor((currentTile - 1) / tilesheetColumns) * SIZE;
+					var tileSheetY = Math.floor((currentTile - 1) / tilesheetColumns) * SIZE;
 
 					switch (currentTile) {
 						case FLOOR:
@@ -180,45 +249,45 @@
 
 	function createOtherObjects() {
 		timeDisplay = Object.create(spriteObject);
-		timeDisplay.sourceX=0;
-		timeDisplay.sourceY=64,
-		timeDisplay.sourceWidth=128;
-		timeDisplay.sourceHeight=48;
+		timeDisplay.sourceX = 0;
+		timeDisplay.sourceY = 64,
+			timeDisplay.sourceWidth = 128;
+		timeDisplay.sourceHeight = 48;
 
-		timeDisplay.width=128;
-		timeDisplay.height=48;
-		timeDisplay.x=canvas.width/2-timeDisplay.width/2;
-		timeDisplay.y=8;
+		timeDisplay.width = 128;
+		timeDisplay.height = 48;
+		timeDisplay.x = canvas.width / 2 - timeDisplay.width / 2;
+		timeDisplay.y = 8;
 		sprites.push(timeDisplay);
 
 		gameOverDisplay = Object.create(spriteObject);
-		gameOverDisplay.sourceX=0;
-		gameOverDisplay.sourceY=129,
-		gameOverDisplay.sourceWidth=316;
-		gameOverDisplay.sourceHeight=290;
+		gameOverDisplay.sourceX = 0;
+		gameOverDisplay.sourceY = 129,
+			gameOverDisplay.sourceWidth = 316;
+		gameOverDisplay.sourceHeight = 290;
 
-		gameOverDisplay.width=316;
-		gameOverDisplay.height=290;
-		gameOverDisplay.x=canvas.width/2-gameOverDisplay.width/2;
-		gameOverDisplay.y=canvas.height/2-gameOverDisplay.height/2;
-		gameOverDisplay.visible=false;
+		gameOverDisplay.width = 316;
+		gameOverDisplay.height = 290;
+		gameOverDisplay.x = canvas.width / 2 - gameOverDisplay.width / 2;
+		gameOverDisplay.y = canvas.height / 2 - gameOverDisplay.height / 2;
+		gameOverDisplay.visible = false;
 		sprites.push(gameOverDisplay);
 
 		gameOverMessage = Object.create(messageObject);
-		gameOverMessage.x=275;
-		gameOverMessage.y=270;
-		gameOverMessage.font='bold 30px Helvetica';
-		gameOverMessage.fillStyle="black";
-		gameOverMessage.text='';
-		gameOverMessage.visible=false;
+		gameOverMessage.x = 275;
+		gameOverMessage.y = 270;
+		gameOverMessage.font = 'bold 30px Helvetica';
+		gameOverMessage.fillStyle = "black";
+		gameOverMessage.text = '';
+		gameOverMessage.visible = false;
 		messages.push(gameOverMessage);
 
 		timerMessage = Object.create(messageObject);
-		timerMessage.x=330;
-		timerMessage.y=10;
-		timerMessage.font='bold 40px Helvetica';
-		timerMessage.fillStyle="white";
-		timerMessage.text='';
+		timerMessage.x = 330;
+		timerMessage.y = 10;
+		timerMessage.font = 'bold 40px Helvetica';
+		timerMessage.fillStyle = "white";
+		timerMessage.text = '';
 		messages.push(timerMessage);
 
 
@@ -226,6 +295,55 @@
 	} //end of createOtherObjects function
 
 	function playGame() {
+		//move alien sprite object
+		if(moveLeft&&!moveRight){
+			alien.vx=-4;
+		}
+
+		if(moveRight&&!moveLeft){
+			alien.vx=4;
+		}
+
+		if(moveUp&&!moveDown){
+			alien.vy=-4;
+		}
+
+		if(moveDown&&!moveUp){
+			alien.vy=4;
+		}
+
+		if(!moveLeft&&!moveRight){
+			alien.vx=0;
+		}
+
+		if(!moveUp&&!moveDown){
+			alien.vy=0;
+		}
+
+		alien.x+=alien.vx;
+		alien.y+=alien.vy;
+
+		//check collsion alien with boxes object
+		for(var i=0;i<boxes.length;i++){
+			blockRectangle(alien,boxes[i]);
+		}
+
+		//check boundaries
+		if(alien.x<64){
+			alien.x=64;
+		}
+
+		if(alien.x+alien.width>canvas.width-64){
+			alien.x=canvas.width-alien.width-64;
+		}
+
+		if(alien.y<64){
+			alien.y=64;
+		}
+
+		if(alien.y+alien.height>canvas.height-64){
+			alien.y=canvas.height-alien.height-64;
+		}
 
 	} //end of playGame function
 
